9798afe09857acbc29803fa016dea1b99be5c073
Harold DRF <95robots@gmail.com>
2019-02-19 23:10:30 -0700

    budget is storage and displayed

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
 //Classes
+class Budget{
+    constructor(budget){
+        this.budget = Number(budget);
+        this.budgetLeft = this.budget;
+    }
+}
 
+//HTML related
+class HTML{
+    insertBudget(amount){
+        budgetTotal.innerHTML = `${amount}`;
+        budgetLeft.innerHTML = `${amount}`;
+    }
+}
 
 //Variables
-const addExpenseForm = document.querySelector('#add-expense');
+const addExpenseForm = document.querySelector('#add-expense'),
+      budgetTotal = document.querySelector('span#total'),
+      budgetLeft = document.querySelector('span#left');
 
 let budget, userBudget;
+
+const html = new HTML();
 
 //Event Listeners
 eventListener();
 function eventListener(){
     document.addEventListener('DOMContentLoaded', function(){
         userBudget = prompt('What\'s your budget for this week? ');
+        //Validate budget
         if(userBudget === null || userBudget === '' || userBudget ==='0' ){
             window.location.reload();
+        }else{
+            budget = new Budget(userBudget);
+
+            html.insertBudget(budget.budget);
         }
     });
 
